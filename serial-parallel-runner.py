2c7a3b38912fc2cd1c5c32b9baf9b9426443762e
Kedar Bhumkar <kedar.bhumkar@gmail.com>
2024-04-15 23:41:43 -0500

    updated

--- serial-parallel-runner.py
+++ serial-parallel-runner.py
@@ -2,39 +2,9 @@ import os
 import asyncio
 from openai import AsyncOpenAI, OpenAI
 import time
+import sys
 
-# small changs to system prompt can have big impact on overall response. Below step-by-step, emotional and objective prompting is used
-system_prompt = """ You are a helpful clinical assistant. Look at the below Transcript from a doctor assessing a patient visit. You need to 
-                    interpret it and return the response as mentioned in the Return_data_constraints section. It should contain the exact keys memtioned in that section. 
-                    Keys are defined before the hyper seperator. Return the response as JSON.
-                    Please do not hallucinate. Do not guess. Cross check your work. Think step by step. If you get this wrong someone's health may get impacted adversly. If you get it correct you will recive $100
-                """
-
-# Define an array of prompts broken by section(s)
-prompt_array = [
-        """Transcript:I visited a patient John Summers on April 1,2024 as part of a Follow up visit a this home. He is an old member of the program.
-           Return_data_constraints: Visit Type - (Comprehensive,Annual,Follow-up,Acute,Acute/Reassessment,Discharge,RN Visit,RN Acute/Reassessment,RN Follow up,Post Transition,Post Hospital,Post Hospital Inpatient,Post Hospital ER/OBS/Outpatient,Superbill,Post Hospital Annual,Risk Stratification Assessment,SNF Initial,SNF Discharge Summary,SNF Interim Discharge Summary,SNF Follow up,SNF Admit,Telephonic RN Follow up,Telephonic Follow-up,Post Hospital Initial,Post Hospital Initial,Quarterly)
-                                    Date of Visit - (should be in Date format , blank)""",
-
-        """Transcript:I visited a patient John Summers on April 1,2024 as part of a Follow up visit a this home. He is an old member of the program.
-           Return_data_constraints:   Member New or Established - (New,Established)
-                                      Place of Service - (Home Nursing Facility, Skilled Nursing Facility, Assisted Living Facility, blank)""",
-        """Transcript:I visited a patient John Summers on April 1,2024 as part of a Follow up visit a this home. He is an old member of the program. He had a  Fall from staircase and had been to the ER.      
-                      He suffers from hypertension, had undergone a CABG 2 years back and has acute bronchotis.
-           Return_data_constraints: Has there been a Fall? - (Yes,No)
-                                    History of present illness - (record here any  medical history mentioned including diseases, conditions which he/she may have had) """                      
-]
-    
-
-prompt_serial = ["""Transcript:I visited a patient John Summers on April 1,2024 as part of a Follow up visit a this home. He is an old member of the program.He is an old member of the program. He had a  Fall from staircase and had been to the ER.      
-                      He suffers from hypertension, had undergone a CABG 2 years back and has acute bronchotis.
-                    Return_data_constraints: Visit Type - (Comprehensive,Annual,Follow-up,Acute,Acute/Reassessment,Discharge,RN Visit,RN Acute/Reassessment,RN Follow up,Post Transition,Post Hospital,Post Hospital Inpatient,Post Hospital ER/OBS/Outpatient,Superbill,Post Hospital Annual,Risk Stratification Assessment,SNF Initial,SNF Discharge Summary,SNF Interim Discharge Summary,SNF Follow up,SNF Admit,Telephonic RN Follow up,Telephonic Follow-up,Post Hospital Initial,Post Hospital Initial,Quarterly)
-                                             Date of Visit - (should be in Date format , blank)
-                                             Member New or Established - (New,Established)
-                                             Place of Service - (Home Nursing Facility, Skilled Nursing Facility, Assisted Living Facility, blank)
-                                             Has there been a Fall? - (Yes,No)
-                                             History of present illness - (record here any  medical history mentioned including diseases, conditions which he/she may have had) 
-                 """]    
+from constants import *
 
 # Async client
 clientAsync = AsyncOpenAI(
@@ -59,9 +29,9 @@ async def async_generate(count, prompt):
     print(f'Parallel response...  {count} ...' , chat_completion.choices[0].message.content)
     return chat_completion.choices[0].message.content
 
-async def generate_concurrently():
+async def generate_concurrently(page):
     
-    tasks = [async_generate(count, prompt_array[count]) for count in range(len(prompt_array))]
+    tasks = [async_generate(count, prompt_parallel_dict[page][count]) for count in range(len(prompt_parallel_dict[page]))]
     # gather returns all the results when all the threads finish execution
     results = await asyncio.gather(*tasks)
     #print(f"results...{results}")
@@ -80,24 +50,35 @@ def sync_generate(count,prompt):
     print(f'\n\n Serial response...  {count} ...' , chat_completion.choices[0].message.content)
     return chat_completion.choices[0].message.content
 
-def generate_serially():    
-    [sync_generate(count, prompt_serial[count]) for count in range(len(prompt_serial))]
+def generate_serially(page):    
+    [sync_generate(count, prompt_serial_dict[page][count]) for count in range(len(prompt_serial_dict[page]))]
     
 
 
-def sync_async_runner():
+def sync_async_runner(page, mode):
     # Parallel invoker
-    start = time.perf_counter()
-    asyncio.run(generate_concurrently())
-    end = time.perf_counter() - start
-    print(f"Parallel Program finished in {end:0.2f} seconds.")
+    if(mode == None):
+        mode = "dual"
 
+    if(page == None):
+        page = "cchpi"     
 
-    # Serial invoker
-    start = time.perf_counter()
-    generate_serially()
-    end = time.perf_counter() - start
-    print(f"Serial Program finished in {end:0.2f} seconds.")
+    if (mode == "parallel" or  mode == "dual"):
+        start = time.perf_counter()    
+        asyncio.run(generate_concurrently(page))
+        end = time.perf_counter() - start
+        print(f"Parallel Program finished in {end:0.2f} seconds.")
 
+    if (mode == "serial" or  mode == "dual"):   
+        # Serial invoker
+        start = time.perf_counter()
+        generate_serially(page)
+        end = time.perf_counter() - start
+        print(f"Serial Program finished in {end:0.2f} seconds.")
 
-[sync_async_runner() for _ in range(1)]
+# Get command-line arguments
+arguments = sys.argv[1:]  # Exclude the script name
+
+# Print the arguments
+print("Command-line arguments:", arguments)
+[sync_async_runner(arguments[0],arguments[1]) for _ in range(1)]
